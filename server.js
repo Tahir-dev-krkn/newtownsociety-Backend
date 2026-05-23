bc4b77eb3348de28a5424c4549707ef7a43ee884
Tahir-dev-krkn <c6hzj2hytx@privaterelay.appleid.com>
2026-05-24 04:10:40 +0530

    Force Gmail SMTP over IPv4

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
+  family: 4,
   connectionTimeout: 10000,
   greetingTimeout: 10000,
   socketTimeout: 10000,
