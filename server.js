f87b3a7ebd04c5785a2cb92c04a6cf210a6d2e15
Tahir-dev-krkn <c6hzj2hytx@privaterelay.appleid.com>
2026-05-24 04:17:33 +0530

    Use Gmail STARTTLS port

--- server.js
+++ server.js
@@ -143,8 +143,9 @@ async function createMailTransporter(){
 
   return nodemailer.createTransport({
     host: smtpHost || "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
+    requireTLS: true,
     connectionTimeout: 10000,
     greetingTimeout: 10000,
     socketTimeout: 10000,
